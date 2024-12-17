5c451fbb8b5b95a37728b49f3dcbcf4038a58b9e
BekzodM27 <bekzodmuqimovfa@gmail.com>
2024-12-17 10:34:31 +0000

    project the end

--- ex6/main.c
+++ ex6/main.c
@@ -1,7 +1,20 @@
-
 #include <stdio.h>
 
-int main(void)
-{
-    printf("Hello World\n");
+int main() {
+    int x, y, M, m, r;
+
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &x, &y);
+
+    M = (x > y) ? x : y;
+    m = (x < y) ? x : y;
+
+    while (m != 0) {
+        r = M % m;
+        M = m;
+        m = r;
+    }
+
+    printf("The greatest common divider is: %d\n", M);
+    return 0;
 }
